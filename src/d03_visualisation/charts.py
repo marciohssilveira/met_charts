416a458c07873750778239dbc6b806d05201898c
Marcio Silveira <marciohssilveira@gmail.com>
2020-12-12 11:08:10 -0300

    cleaned charts.py

--- src/d03_visualisation/charts.py
+++ src/d03_visualisation/charts.py
@@ -8,6 +8,7 @@ import pandas as pd
 from datetime import datetime
 from metpy.units import units
 
+
 from d02_processing.indices import CalculateIndices
 from d02_processing.variables import ExtractVariables
 
@@ -679,119 +680,6 @@ class CalculateCharts:
             f'Instabilidade {self.time_stamp}', fontsize=16, ha='center')
         plt.savefig(
             f'{self.output_path}/instabilidade_{self.time_step:02d}.jpg')
-
-    # def temperature_advection(self):
-    #     # Pull out variables you want to use
-    #     hght_var = self.data.variables['Geopotential_height_isobaric']
-    #     temp_var = self.data.variables['Temperature_isobaric']
-    #     u_wind_var = self.data.variables['u-component_of_wind_isobaric']
-    #     v_wind_var = self.data.variables['v-component_of_wind_isobaric']
-    #     lat_var = self.data.variables['lat']
-    #     lon_var = self.data.variables['lon']
-
-    #     # Get actual data values and remove any size 1 dimensions
-    #     lat = lat_var[:].squeeze()
-    #     lon = lon_var[:].squeeze()
-    #     hght = hght_var[:].squeeze()
-    #     temp = units.Quantity(temp_var[:].squeeze(), temp_var.units)
-    #     u_wind = units.Quantity(u_wind_var[:].squeeze(), u_wind_var.units)
-    #     v_wind = units.Quantity(v_wind_var[:].squeeze(), v_wind_var.units)
-
-    #     lev_850 = np.where(self.data.variables['isobaric'][:] == 850 * 100)[0][0]
-    #     hght_850 = hght[lev_850]
-    #     temp_850 = temp[lev_850]
-    #     u_wind_850 = u_wind[lev_850]
-    #     v_wind_850 = v_wind[lev_850]
-
-    #     # Combine 1D latitude and longitudes into a 2D grid of locations
-    #     lon_2d, lat_2d = np.meshgrid(lon, lat)
-
-    #     # Gridshift for barbs
-    #     lon_2d[lon_2d > 180] = lon_2d[lon_2d > 180] - 360
-
-    #     #########################################
-
-    #     # Use helper function defined above to calculate distance
-    #     # between lat/lon grid points
-    #     dx, dy = mpcalc.lat_lon_grid_deltas(lon_var, lat_var)
-
-    #     # Calculate temperature advection using metpy function
-    #     adv = mpcalc.advection(temp_850, [u_wind_850, v_wind_850],
-    #                            (dx, dy), dim_order='yx')
-
-    #     # Smooth heights and advection a little
-    #     # Be sure to only put in a 2D lat/lon or Y/X array for smoothing
-    #     Z_850 = mpcalc.smooth_gaussian(hght_850, 2)
-    #     adv = mpcalc.smooth_gaussian(adv, 2)
-
-    #     # Set Projection of Data
-    #     datacrs = ccrs.PlateCarree()
-
-    #     # Set Projection of Plot
-    #     plotcrs = ccrs.cartopy.crs.Mercator(central_longitude=0.0,
-    #                                         min_latitude=-80.0,
-    #                                         max_latitude=84.0,
-    #                                         globe=None,
-    #                                         latitude_true_scale=None,
-    #                                         false_easting=0.0,
-    #                                         false_northing=0.0,
-    #                                         scale_factor=None)
-
-    #     # Create new figure
-    #     fig = plt.figure(figsize=(10, 12))
-    #     gs = gridspec.GridSpec(2, 1, height_ratios=[1, .02], bottom=.07, top=.99,
-    #                            hspace=0.01, wspace=0.01)
-
-    #     # Add the map and set the extent
-    #     ax = plt.subplot(gs[0], projection=plotcrs)
-    #     # plt.title(f'850mb Temperature Advection for {time:%d %B %Y %H:%MZ}', fontsize=16)
-    #     ax.set_extent([-30., -80., 0., -50.])
-
-    #     # Plot Height Contours
-    #     clev850 = np.arange(900, 3000, 30)
-    #     cs = ax.contour(lon_2d, lat_2d, Z_850, clev850, colors='black', linewidths=1.5,
-    #                     linestyles='solid', transform=datacrs)
-    #     plt.clabel(cs, fontsize=10, inline=1, inline_spacing=10, fmt='%i',
-    #                rightside_up=True, use_clabeltext=True)
-
-    #     # Plot Temperature Contours
-    #     clevtemp850 = np.arange(-20, 20, 2)
-    #     cs2 = ax.contour(lon_2d, lat_2d, temp_850.to(units('degC')), clevtemp850,
-    #                      colors='green', linewidths=1.25, linestyles='dashed',
-    #                      transform=datacrs)
-    #     plt.clabel(cs2, fontsize=10, inline=1, inline_spacing=10, fmt='%i',
-    #                rightside_up=True, use_clabeltext=True)
-
-    #     # Plot Colorfill of Temperature Advection
-    #     cint = np.arange(-8, 9)
-    #     cf = ax.contourf(lon_2d, lat_2d, 3 * adv.to(units('delta_degC/hour')), cint[cint != 0],
-    #                      extend='both', cmap='bwr', transform=datacrs)
-        # cax = plt.subplot(gs[1])
-        # cb = plt.colorbar(cf, cax=cax, orientation='horizontal', extendrect=True, ticks=cint)
-        # cb.set_label(r'$^{o}C/3h$', size='large')
-
-        # # Add state/country boundaries to plot
-        # states_provinces = cfeature.NaturalEarthFeature(
-        #     category='cultural',
-        #     name='admin_1_states_provinces_lines',
-        #     scale='50m',
-        #     facecolor='none')
-
-        # SOURCE = 'Natural Earth'
-        # LICENSE = 'public domain'
-
-        # ax.add_feature(cfeature.LAND)
-        # ax.add_feature(cfeature.COASTLINE)
-        # ax.add_feature(cfeature.BORDERS)
-        # ax.add_feature(states_provinces, edgecolor='gray')
-        # ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
-
-        # # Plot Wind Barbs
-        # ax.barbs(lon_2d, lat_2d, u_wind_850.magnitude, v_wind_850.magnitude,
-        #          length=6, regrid_shape=20, pivot='middle', transform=datacrs)
-
-    #     plt.savefig('test.png')
-    #     plt.show()
 
 
 if __name__ == "__main__":
